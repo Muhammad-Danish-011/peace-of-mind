fe3db59b4e4ded919db27a7d89ebf834432f09fb
sarahp27 <sarah.patel@xloopdigital.com>
2023-06-08 15:07:54 +0500

    cards updated

---
+++ src/Components/MiniCard.jsx
@@ -0,0 +1,68 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+const card = (
+ <React.Fragment>
+    <CardContent style={{backgroundColor: "rgb(	207	227	223)",
+              width:'210px',
+              justifyContent:"center",
+              borderRadius:'20px',
+              alignItems:"center",
+              padding:"10px",
+              paddingRight:"20px",
+              paddingLeft:"20px",
+              height:"60px"
+            //   display: "flex"
+             }}>
+
+      <Typography variant="h5" fontSize="15px" component="div" display="flex" >
+       Aoun Ali 
+
+       <Button
+      variant='outlined'
+      sx= {{color: 'black',
+            borderRadius:'20px', 
+            display:'flex',
+            marginLeft:'50px',
+            fontSize:'8px',
+            padding:'8px',
+            bgcolor: 'white'}}
+      size="small" >Book Now</Button>
+      </Typography>
+     
+      <Typography sx={{ mb: 1.5 }} color="text.secondary" fontSize="10px"
+      marginTop="-10px">
+        Social Psychologist
+      </Typography>
+      
+    
+    </CardContent>
+  </React.Fragment>
+);
+
+export default function OutlinedCard() {
+  return (
+     <Box
+     width='300px'
+    display='flex'
+    alignItems="center"
+     margin="auto"
+
+    sx={{
+        borderRadius:'30px',
+        
+    }}
+    
+    >
+      <Card style={{"borderRadius":'20px',
+    padding:"0px"}}>{card}</Card>
+    </Box>
+  );
+}
+
+
+
