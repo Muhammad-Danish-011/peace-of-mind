56b7abebf700e063fab2c9302ca066843e23d417
fatimabatooll <fatima.batool@xloopdigital.com>
2023-06-09 11:13:56 +0500

    home screen chnages

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react';
 import { Box } from '@mui/material';
 import Search from '../../components/patient/Search';
@@ -92,3 +93,38 @@ const Home = () => {
 export default Home;
 
 
+=======
+import React from 'react'
+import Search from '../../components/Search'
+import TappointLink from '../../components/TappointLink'
+import Tappoint from '../../components/Tappoint'
+import Prevappoints from '../../components/Prevappoints'
+import MiniCard from '../../Components/MiniCard'
+
+const Home = () => {
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <div className='center' style={{ marginTop: '20px', display: 'flex' }}>
+        <Search/>
+        <TappointLink/>
+      </div>
+      <div style={{
+        marginTop: '-83px',
+        width: '100%',
+        maxWidth: '800px',
+        padding: '10px',
+        display: 'flex',
+        flexDirection: 'column', // new line
+        alignItems: 'center',
+        backgroundColor:'#8FB3AC'
+      }}>
+        <Tappoint />
+        <Prevappoints />
+        <MiniCard />
+      </div>
+    </div>
+  )
+}
+
+export default Home
+>>>>>>> 6af1896 (home screen chnages)
