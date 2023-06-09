6af1896536e849745986ba1ddd8f0859440baf5a
fatimabatooll <fatima.batool@xloopdigital.com>
2023-06-09 11:13:56 +0500

    home screen chnages

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,48 +4,29 @@ import TappointLink from '../../components/TappointLink'
 import Tappoint from '../../components/Tappoint'
 import Prevappoints from '../../components/Prevappoints'
 import MiniCard from '../../Components/MiniCard'
-import CssBaseline from '@mui/material/CssBaseline';
-import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
-
 
 const Home = () => {
   return (
-    <>
-    <React.Fragment>
-  <CssBaseline />
-  <Container 
-    sx={{ 
-      position: 'absolute', 
-      top: 0, 
-      left: '90px', // adjust this value as needed
-      width: ' 800px auto', 
-      margin: '0px auto 0', // adjust this value as needed
-      backgroundColor: '#cfe8fc', 
-      height: '80vh' 
-    }}
-  >
-    {/* Your content goes here */}
-  </Container>
-</React.Fragment>
-
-
-    {/*  */}
-
-    <div  style={{ position: 'absolute', 
-    display:'flex',
-    flexDirection:'column',
-    right: '0px', 
-    width: '300px', 
-    border: '3px ', 
-    padding: '10px',
-    backgroundColor:'#8FB3AC' }}>
-  <Tappoint />
-  <Prevappoints />
-  <MiniCard />
-</div>
-
-    </>
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <div className='center' style={{ marginTop: '20px', display: 'flex' }}>
+        <Search/>
+        <TappointLink/>
+      </div>
+      <div style={{
+        marginTop: '-83px',
+        width: '100%',
+        maxWidth: '800px',
+        padding: '10px',
+        display: 'flex',
+        flexDirection: 'column', // new line
+        alignItems: 'center',
+        backgroundColor:'#8FB3AC'
+      }}>
+        <Tappoint />
+        <Prevappoints />
+        <MiniCard />
+      </div>
+    </div>
   )
 }
 
