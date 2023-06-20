502f2014886ef93421ef4d56033ec801bdd883ff
fatimabatooll <fatima.batool@xloopdigital.com>
2023-06-20 16:15:25 +0500

    councler profile in progress

--- src/pages/calendar/Calendar.jsx
+++ src/pages/calendar/Calendar.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
-import SurveyModal from '../../components/SurveyModal'
-import CounclerProfile from '../counclerprofile/CounclerProfile'
 
 
 
 const Calendar = () => {
   return (
     <div>
-  {/* <SurveyModal/> */}
-  <CounclerProfile/>
     </div>
   )
 }
