edcd0231e3f864c09b5e10e49d87b8159a2d2661
Atia Khan <atia.khan@xloopdigital.com>
2023-06-20 09:19:10 +0500

    ...

--- src/index.js
+++ src/index.js
@@ -124,40 +124,14 @@ import UserProfile from './Avengers404/Pages/UserProfile';
 
 import Privateroute from './Avengers404/components/Privateroute';
 import { AuthProvider } from './Avengers404/Pages/AuthContext';
-<<<<<<< HEAD
-// import { BrowserRouter } from "react-router-dom";
-// import SignupForm from "./Avengers404/Pages/SignupPage";
-// import Loginform from "./Avengers404/Pages/LoginPage";
-// import NewPassword from "./Avengers404/Pages/NewPassword";
-// import ForgetPassword from "./Avengers404/Pages/ForgotPassword";
-// import Dashboard from "./Avengers404/Pages/dashboard";
-// import { AuthProvider } from "./Avengers404/Pages/AuthContext";
-// import { Route, Routes } from "react-router-dom";
-// import { createRoot } from "react-dom/client";
-// import Home from "./Avengers404/Pages/Home";
-
-// import Privateroute from "./Avengers404/components/Privateroute";
-// import UserProfile from "./Avengers404/Pages/UserProfile";
-createRoot(document.getElementById("root")).render(
-=======
 
 
-// import { BrowserRouter } from "react-router-dom";
-// import SignupForm from "./Avengers404/Pages/SignupPage";
-// import Loginform from "./Avengers404/Pages/LoginPage";
-// import NewPassword from "./Avengers404/Pages/NewPassword";
-// import ForgetPassword from "./Avengers404/Pages/ForgotPassword";
-// import Dashboard from "./Avengers404/Pages/dashboard";
-// import { AuthProvider } from "./Avengers404/Pages/AuthContext";
-// import { Route, Routes } from "react-router-dom";
-// import { createRoot } from "react-dom/client";
-// import Home from "./Avengers404/Pages/Home";
 
-// import Privateroute from "./Avengers404/components/Privateroute";
-// import UserProfile from "./Avengers404/Pages/UserProfile";
+
+
 createRoot(document.getElementById("root")).render(
 
->>>>>>> 2382503 (...)
+
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
