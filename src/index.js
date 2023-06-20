f9d3ee28139b151eb38b2c95d59a6335d0324e85
mohammadsabih6 <sabih.tariq@xloopdigital.com>
2023-06-20 08:45:25 +0500

    updated

--- src/index.js
+++ src/index.js
@@ -125,20 +125,19 @@ import UserProfile from './Avengers404/Pages/UserProfile';
 
 import Privateroute from './Avengers404/components/Privateroute';
 import { AuthProvider } from './Avengers404/Pages/AuthContext';
-import React from "react";
-import { BrowserRouter } from "react-router-dom";
-import SignupForm from "./Avengers404/Pages/SignupPage";
-import Loginform from "./Avengers404/Pages/LoginPage";
-import NewPassword from "./Avengers404/Pages/NewPassword";
-import ForgetPassword from "./Avengers404/Pages/ForgotPassword";
-import Dashboard from "./Avengers404/Pages/dashboard";
-import { AuthProvider } from "./Avengers404/Pages/AuthContext";
-import { Route, Routes } from "react-router-dom";
-import { createRoot } from "react-dom/client";
-import Home from "./Avengers404/Pages/Home";
+// import { BrowserRouter } from "react-router-dom";
+// import SignupForm from "./Avengers404/Pages/SignupPage";
+// import Loginform from "./Avengers404/Pages/LoginPage";
+// import NewPassword from "./Avengers404/Pages/NewPassword";
+// import ForgetPassword from "./Avengers404/Pages/ForgotPassword";
+// import Dashboard from "./Avengers404/Pages/dashboard";
+// import { AuthProvider } from "./Avengers404/Pages/AuthContext";
+// import { Route, Routes } from "react-router-dom";
+// import { createRoot } from "react-dom/client";
+// import Home from "./Avengers404/Pages/Home";
 
-import Privateroute from "./Avengers404/components/Privateroute";
-import UserProfile from "./Avengers404/Pages/UserProfile";
+// import Privateroute from "./Avengers404/components/Privateroute";
+// import UserProfile from "./Avengers404/Pages/UserProfile";
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
