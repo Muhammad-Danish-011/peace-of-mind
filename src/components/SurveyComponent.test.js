8ef6aff15db4899da6dac626b5982d9b2b0d277f
Atique <muhammadatique444@gmail.com>
2023-06-27 13:24:45 +0500

    formating

--- src/components/SurveyComponent.test.js
+++ src/components/SurveyComponent.test.js
@@ -9,151 +9,148 @@ import SurveyModal from './SurveyModal';
 global.ResizeObserver = ResizeObserver;
 
 jest.mock('react-s3', () => ({
-    uploadFile: jest.fn(),
-  }));
+  uploadFile: jest.fn(),
+}));
 
-  describe("Survey component", () => {
-
-
-    it('renders modal icon', () => {
-        render(<SurveyModal />);
-    
-        // Use appropriate selector to target the modal icon element
-        const modalIcon = screen.getByRole({name:'InsertDriveFileRoundedIcon'});
-    
-        // Assert that the modal icon element is in the document
-        expect(modalIcon).toBeInTheDocument();
-      });
-
-    it("renders survey header correctly", () => {
-        render(
-            <SurveyComponent />
-        );
-        expect(screen.getByText("Mental Health Intake Form")).toBeInTheDocument();
-        expect(screen.getByText("It's okay, not to be okay.")).toBeInTheDocument();
-
-    });
-
-    it("renders survey note correctly", () => {
-        render(
-            <SurveyComponent />
-        );
-        expect(screen.getByText("Note:")).toBeInTheDocument();
-        expect(screen.getByText("By participating in this survey, you will play a vital role in helping us understand your needs, all you data will remain private and confidential.")).toBeInTheDocument();
-    })
+describe("Survey component", () => {
 
 
-    //q1
+  it('renders modal icon', () => {
+    render(<SurveyModal />);
 
-    test('Renders survey form Q1', () => {
-        render(<SurveyComponent />);
+    const modalIcon = screen.getByRole({ name: 'InsertDriveFileRoundedIcon' });
+    expect(modalIcon).toBeInTheDocument();
+  });
 
-        const question1 = screen.getByLabelText(/^Are you currently\s\*$/i);
+  it("renders survey header correctly", () => {
+    render(
+      <SurveyComponent />
+    );
+    expect(screen.getByText("Mental Health Intake Form")).toBeInTheDocument();
+    expect(screen.getByText("It's okay, not to be okay.")).toBeInTheDocument();
 
-        expect(question1).toBeInTheDocument();
-    });
+  });
 
-    //q2
-
-
-     test('Renders survey form Q2', () => {
-         render(<SurveyComponent />);
-
-         const question2 = screen.getByLabelText(/^Are you\s\*$/i);
-
-         expect(question2).toBeInTheDocument();
-     });
-
-    //q3
-    it('renders survey form Q3', () => {
-        render(<SurveyComponent />);
-        const question3 = screen.getByText(/Primary Care Physician/i);
-        expect(question3).toBeInTheDocument();
-    });
-
-    //q4
-
-    it('renders survey form Q4', () => {
-        render(<SurveyComponent />);
-        const question4 = screen.getByText(/Current Therapist \/ Counselor/i);
-        expect(question4).toBeInTheDocument();
-    });
+  it("renders survey note correctly", () => {
+    render(
+      <SurveyComponent />
+    );
+    expect(screen.getByText("Note:")).toBeInTheDocument();
+    expect(screen.getByText("By participating in this survey, you will play a vital role in helping us understand your needs, all you data will remain private and confidential.")).toBeInTheDocument();
+  })
 
 
-    //q5
-    it('renders survey form Q5', () => {
-        render(<SurveyComponent />);
-        const question5 = screen.getByText(/Therapist's Phone Number/i);
-        expect(question5).toBeInTheDocument();
-    });
+  //q1
 
-    //q6
+  test('Renders survey form Q1', () => {
+    render(<SurveyComponent />);
 
-    it('renders survey form Q6', () => {
-        render(<SurveyComponent />);
-        const question6 = screen.getByLabelText(/Please list the problem\(s\) which you are seeking help\? /i);
-        expect(question6).toBeInTheDocument();
-    });
+    const question1 = screen.getByLabelText(/^Are you currently\s\*$/i);
 
-    
+    expect(question1).toBeInTheDocument();
+  });
 
-test('Tests the presence of required field "Current Symptoms *"', () => {
-  render(<SurveyComponent />);
-  
-  const question7 = screen.getByLabelText(/Current Symptoms \*/i);
-  
-  expect(question7).toBeInTheDocument();
-});
+  //q2
 
 
+  test('Renders survey form Q2', () => {
+    render(<SurveyComponent />);
 
-test('Question about feelings or thoughts of not wanting to live is rendered', () => {
-  render(<SurveyComponent />);
-  
-  const question8 = screen.getByLabelText(/Have you ever had feelings or thoughts that you didn't want to live\? \*/);
-  
-  expect(question8).toBeInTheDocument();
-});
+    const question2 = screen.getByLabelText(/^Are you\s\*$/i);
 
+    expect(question2).toBeInTheDocument();
+  });
 
+  //q3
+  it('renders survey form Q3', () => {
+    render(<SurveyComponent />);
+    const question3 = screen.getByText(/Primary Care Physician/i);
+    expect(question3).toBeInTheDocument();
+  });
 
-test('Question about feeling not wanting to live is rendered', () => {
-  render(<SurveyComponent />);
-  
-  const questionLabel = screen.getByLabelText(/Do you currently feel that you don't want to live\? \*/i);
-  
-  expect(questionLabel).toBeInTheDocument();
-});
+  //q4
 
-
-
-test('Question "How often do you have these thoughts?" is matched correctly', () => {
-  render(<SurveyComponent />);
-  
-  const questionLabel = screen.getByLabelText(/How often do you have these thoughts\? \*/i);
-  
-  expect(questionLabel).toBeInTheDocument();
-});
+  it('renders survey form Q4', () => {
+    render(<SurveyComponent />);
+    const question4 = screen.getByText(/Current Therapist \/ Counselor/i);
+    expect(question4).toBeInTheDocument();
+  });
 
 
+  //q5
+  it('renders survey form Q5', () => {
+    render(<SurveyComponent />);
+    const question5 = screen.getByText(/Therapist's Phone Number/i);
+    expect(question5).toBeInTheDocument();
+  });
 
-test('Question about feeling hopeless and/or worthless is rendered', () => {
-  render(<SurveyComponent />);
-  
-  const questionLabel = screen.getByLabelText(/Do you feel hopeless and\/or worthless\? \*/i);
-  
-  expect(questionLabel).toBeInTheDocument();
-});
+  //q6
+
+  it('renders survey form Q6', () => {
+    render(<SurveyComponent />);
+    const question6 = screen.getByLabelText(/Please list the problem\(s\) which you are seeking help\? /i);
+    expect(question6).toBeInTheDocument();
+  });
+
+  //q7
+
+  it('Tests the presence of required field "Current Symptoms *"', () => {
+    render(<SurveyComponent />);
+
+    const question7 = screen.getByLabelText(/Current Symptoms \*/i);
+
+    expect(question7).toBeInTheDocument();
+  });
+
+  //q8
+
+  it('Question about feelings or thoughts of not wanting to live is rendered', () => {
+    render(<SurveyComponent />);
+
+    const question8 = screen.getByLabelText(/Have you ever had feelings or thoughts that you didn't want to live\? \*/);
+
+    expect(question8).toBeInTheDocument();
+  });
+
+  //9
+
+  it('Question about feeling not wanting to live is rendered', () => {
+    render(<SurveyComponent />);
+
+    const questionLabel = screen.getByLabelText(/Do you currently feel that you don't want to live\? \*/i);
+
+    expect(questionLabel).toBeInTheDocument();
+  });
+
+  //10
+
+  it('Question "How often do you have these thoughts?" is matched correctly', () => {
+    render(<SurveyComponent />);
+
+    const questionLabel = screen.getByLabelText(/How often do you have these thoughts\? \*/i);
+
+    expect(questionLabel).toBeInTheDocument();
+  });
+
+  //11
+
+  it('Question about feeling hopeless and/or worthless is rendered', () => {
+    render(<SurveyComponent />);
+
+    const questionLabel = screen.getByLabelText(/Do you feel hopeless and\/or worthless\? \*/i);
+
+    expect(questionLabel).toBeInTheDocument();
+  });
 
 
-    //next button
+  //next button
 
-    test('Button is matched correctly', () => {
-        render(<SurveyComponent />);
+  test('Button is matched correctly', () => {
+    render(<SurveyComponent />);
 
-        const buttonElement = screen.getByText(/next/i);
+    const buttonElement = screen.getByText(/next/i);
 
-        expect(buttonElement).toBeInTheDocument();
-    });
+    expect(buttonElement).toBeInTheDocument();
+  });
 
 });
