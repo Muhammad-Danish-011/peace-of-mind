30ae7cc4d1fe7ce7391e1e70aedbeb2382cc1339
fatimabatooll <fatima.batool@xloopdigital.com>
2023-07-06 08:08:10 +0500

    updated

--- src/global/Navbar.jsx
+++ src/global/Navbar.jsx
@@ -14,6 +14,7 @@ const Navbar = ({ handleSidebarToggle }) => {
               <IconButton
                 color="inherit"
                 aria-label="open sidebar"
+                onClick={handleSidebarToggle}
                 edge="start"
               >
                 <MenuIcon style={{ fontSize: '48px' }}/>
